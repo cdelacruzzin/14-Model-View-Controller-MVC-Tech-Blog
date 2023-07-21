91d72ae61053c1adec7d0089ece1a5eaa0afa38d
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-21 19:52:22 -0400

    created a fetch to get the `/` page from the controllers

--- develop/public/js/loginController.js
+++ develop/public/js/loginController.js
@@ -1,4 +1,12 @@
 
+const  loginHandler = async (event) => {
+    event.prevendDefault();
+
+    await fetch('/', {
+        method: 'GET',
+    });
+    
+}
 
 document.querySelector('#logninBtn').addEventListener('click', loginHandler);
 
