96dc9c28cd4cddba23b45e7ee14095feb2ca1b7b
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-21 16:18:25 -0400

    created a fetch request to /api/users/login

--- develop/public/js/login.js
+++ develop/public/js/login.js
@@ -1,6 +1,18 @@
 const loginForm = async (event) => {
     event.preventDefault();
 
+    //collects email and password value from login form
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
+
+    //if both email and password are entered, a post request will be sent to the api
+    if(email && password) {
+        const response = await fetch('/api/users/login', {
+            method: POST,
+            body: JSON.stringify({email, password}),
+            headers: {'Content-Type': 'application/json'},
+        });
+    }
+
+
 }
