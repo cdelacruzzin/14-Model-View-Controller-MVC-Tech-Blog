a7feef5582a234b450319a8bcc0c56a54f7c5a8d
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-22 13:21:39 -0400

    post method to /signup route to create an account

--- develop/controllers/api/UserRoutes.js
+++ develop/controllers/api/UserRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {User} = require('../../models/User');
+const User = require('../../models/User');
 
 
 // Login - /api/users/login
@@ -11,5 +11,25 @@ router.post('/login', async (req, res) => {
 // CREATE new user - /api/users/signup
 
 router.post('/signup', async (req, res) => {
-    
+    try {
+        // const userData = await User.create(req.body);
+        const newUser = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.status(200).json(newUser);
+
+        // req.session.save(() => {
+        //     req.session.user_id = User.id;
+        //     req.session.loggedIn = true;
+
+        //     
+        // });
+
+    } catch (error) {
+        res.status(400).json(error);
+    }
 })
+
+module.exports = router;
