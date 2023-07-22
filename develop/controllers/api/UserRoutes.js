a63254dae270a5ce6524764a374a669f0572794f
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-21 22:50:52 -0400

    created post routes for login and signup

---
+++ develop/controllers/api/UserRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const {User} = require('../../models/User');
+
+
+// Login - /api/users/login
+router.post('/login', async (req, res) => {
+
+})
+
+
+// CREATE new user - /api/users/signup
+
+router.post('/signup', async (req, res) => {
+    
+})
