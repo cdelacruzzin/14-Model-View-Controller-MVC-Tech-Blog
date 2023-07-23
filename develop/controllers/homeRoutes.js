d05186ade7e5d6cce6f614da45011ca60b6c3cba
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-22 23:27:06 -0400

    comment to the homepage query method

--- develop/controllers/homeRoutes.js
+++ develop/controllers/homeRoutes.js
@@ -19,20 +19,22 @@ router.get('/signup', async (req, res) => {
     res.render('./login/signup');
 });
 
-router.get('/homepage', async (req, res) => {
+
+
+router.get('/homepage', async (req, res) => {   //Query all blogs from the database using the Sequelize `findAll` method
     try {
         const allBlogs = await Blog.findAll({
-            include: [
+            include: [  //includes the User model in the query
                 {
                     model: User,
-                    attributes: ['name'],
+                    attributes: ['name'],   //specify that to include the 'name' attribure of the user
 
                 },
             ],
         });
 
-        const blogs = allBlogs.map((blog) => blog.get({plain:true}));
-        console.log(blogs);
+        const blogs = allBlogs.map((blog) => blog.get({plain:true})); //maps over all elements of of allBlogs and serialeze them
+        //serialize to make the data easier to handle/reaD
         
         res.render('homepage', {
             blogs,
