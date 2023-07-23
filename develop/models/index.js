cb709fa140b5530db70b9eb4731a2f31a1893873
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-23 01:08:30 -0400

    create new blog entries in the Blog table from the BlogData.json elements

--- develop/models/index.js
+++ develop/models/index.js
@@ -2,11 +2,13 @@ const User = require('./User');
 const Blog = require('./Blog');
 
 
+// defines one to many relations between user and blog
 User.hasMany(Blog, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE',
 });
 
+//defines a one-to-one relation between blog to user
 Blog.belongsTo(User, {
     foreignKey: 'user_id',
 })
--- develop/seeds/seed.js
+++ develop/seeds/seed.js
@@ -1,20 +1,27 @@
 const sequelize = require('../config/connection');
-const {User} = require('../models');
+const { User, Blog } = require('../models');
 
 const userData = require('./userData.json');
+const BlogData = require('./blogData.json');
 
 const seedData = async () => {
-    await sequelize.sync({force:true});     //syncs all the defined models to the database
-    // force: true will drop the defined tables in the db and create them again.
+  await sequelize.sync({ force: true });     //syncs all the defined models to the database
+  // force: true will drop the defined tables in the db and create them again.
 
+  //
+  const allUsers = await User.bulkCreate(userData, {  //creates multiple instances where each element of the userData array represents a new row in the database
+    individualHooks: true,  //ensures that each hooks are created for each row being created.
+    returning: true,
+  });
 
-    //
-    const allUsers = await User.bulkCreate(userData, {  //creates multiple instances where each element of the userData array represents a new row in the database
-        individualHooks: true,  //ensures that each hooks are created for each row being created.
-        returning: true,
-      });
-
-      process.exit(0);
+  // for each element of the BlogData array, uses sequelize to create a new entry in the Blog db.
+  for (const blog of BlogData){ 
+    await Blog.create({
+      ...blog,  // uses the spread operator to copy all properties from the blog object into the new Blog entry
+      user_id: allUsers[Math.floor(Math.random() * allUsers.length)].id,  // assigns random user's id to the user_id of the Blog entry
+    });
+  }
+  process.exit(0);
 
 };
 
