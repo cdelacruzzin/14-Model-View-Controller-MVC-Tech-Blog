4f494f21f96a8ed2f529fc95e3c4b10a5604a6ae
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-24 16:07:31 -0400

    created a blog.js to handle new comments

---
+++ develop/controllers/api/blogRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {Blog} = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/',withAuth , async (req, res) => {
+    try {
+        const newBlog = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newBlog);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
---
+++ develop/public/js/blog.js
@@ -0,0 +1,7 @@
+const newCommentHandler = async (event) => {
+    event.preventDefault();
+
+    
+}
+
+document.querySelector('.comment-form').addEventListener('submit', newCommentHandler)
